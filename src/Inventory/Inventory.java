008272567b93d3a02a216b66c510d9e0253ca685
Ahmed Mohamed <ahmedmidoria6@gmail.com>
2025-07-08 20:12:54 +0300

    intail commit

---
+++ src/Inventory/Inventory.java
@@ -0,0 +1,29 @@
+package Inventory;
+import Entities.*;
+Import Interfaces.*;
+
+public class Inventory {
+    private List<IBook> books = new ArrayList<>();
+
+    public void addBook(IBook book) {
+        books.add(book);
+    }
+
+    public void removeOutdatedBooks(int maxAge, int currentYear) {
+        books.removeIf(book -> currentYear - book.getPublishYear() > maxAge);
+    }
+
+    public IBook findByISBN(String isbn) {
+        for (IBook book : books) {
+            if (book.getISBN().equals(isbn)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public List<IBook> getAllBooks() {
+        return books;
+    }
+}
+
